d7d74e88545fb3484d32b6ad3dd02f3ffdbeed86
MaximillianVoss <fosswolfgang@hotmail.com>
2014-01-15 22:19:22 +0400

    Currency project
    Currency sample project added

---
+++ Currency/Currency/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Currency
+//
+//  Created by Александр Чайковский on 07.12.13.
+//  Copyright (c) 2013 Microsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
